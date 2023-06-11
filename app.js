d53beb5a8b1024051b3e84a7b8b3701c2aa1f710
Sashank Rampalli <66544316+Sashankr@users.noreply.github.com>
2023-06-11 17:32:36 +0530

    First api using nodejs

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/api/products", (req, res) => {
+  res.json([
+    { name: "iPhone", price: 600 },
+    { name: "iPad", price: 700 },
+    { name: "iWatch", price: 800 },
+  ]);
+});
+
 app.listen(port, () => {
   console.log("Server running on port: " + port);
 });
