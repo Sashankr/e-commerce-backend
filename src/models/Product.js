06ac25e91b6be1dbb4e09832dcf3395db61b2290
Sashankr <sashankr97@gmail.com>
2023-06-11 20:06:11 +0530

    updated types for user and product

--- src/models/Product.js
+++ src/models/Product.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
-    desc: { type: String, required: true, unique: true },
-    img: { type: String, required: true, unique: true },
-    categories: { type: String, required: true, unique: true },
-    size: { type: String, required: true, unique: true },
-    color: { type: String, required: true, unique: true },
-    price: { type: String, required: true, unique: true },
+    desc: { type: String, required: true },
+    img: { type: String, required: true },
+    categories: { type: Array },
+    size: { type: String },
+    color: { type: String },
+    price: { type: Number, required: true },
   },
   { timestamps: true }
 );
