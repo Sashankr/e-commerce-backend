4f849039f1d580f9081014141a9210cd6c5a873f
Sashankr <sashankr97@gmail.com>
2023-06-11 20:17:39 +0530

    configured express endpoints to point to respective routes defined in app.js

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./src/routes/user");
+const productRoutes = require("./src/routes/product");
+const cartRoutes = require("./src/routes/cart");
+const orderRoutes = require("./src/routes/order");
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/carts", cartRoutes);
 
 mongoose
   .connect(process.env.MONGO_URL)
